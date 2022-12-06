ef01502b3673396f34b000146c3465a06fcca248
toseef31 <peekinternational@gmail.com>
2022-12-06 18:16:09 +0500

    click on venue

--- app.js
+++ app.js
@@ -31,13 +31,13 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
-});
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many requests from this IP, please try again in an hour!",
+// });
 
-app.use("/api", limiter);
+// app.use("/api", limiter);
 app.use(express.json({ limit: "10kb" }));
 app.use(mongoSanitize());
 app.use(xss());

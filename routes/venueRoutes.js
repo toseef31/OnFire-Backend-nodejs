bdfc29c84ef979947cfd9fc3e7fc487949928325
toseef31 <peekinternational@gmail.com>
2022-12-06 12:44:01 +0500

    api added in venue

--- routes/venueRoutes.js
+++ routes/venueRoutes.js
@@ -4,5 +4,6 @@ const venueController = require("./../controllers/venueController");
 const router = express.Router();
 
 router.get("/distances/:latlng/unit/:unit", venueController.getallnearvenues);
+router.get("/getallbycity", venueController.getvenuesbycitycountry);
 router.get("/getallvenue", venueController.getallvenues);
 module.exports = router;
